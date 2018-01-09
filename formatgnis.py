22cd26f7b7990b0fffd14622aaf0b1838d2a0b73
Max Erickson <maxerickson@gmail.com>
2018-01-09 17:10:18 -0500

    Add GNIS, wikidata and wikipedia tags.

---
+++ formatgnis.py
@@ -0,0 +1,42 @@
+import csv
+import argparse
+
+wikidata=dict()
+with open("wikidata.csv") as wiki:
+    rdr=csv.reader(wiki)
+    for row in rdr:
+        if len(row)==5:
+            item,title,label,county,gnis=row
+            item=item[len("http://www.wikidata.org/entity/"):]
+            wikidata[gnis]=item,title
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Process GNIS pipe separated file for use with OGR.')
+    parser.add_argument('infile',
+                        help='Input file name')
+    parser.add_argument('outfile',
+                        help='Output file name')
+    args = parser.parse_args()
+    with open(args.infile) as inf:
+        reader=csv.reader(inf, delimiter="|")
+        with open(args.outfile, 'w') as ouf:
+            writer=csv.writer(ouf, delimiter="\t")
+            header=next(reader)
+            header[0]="FeatureName"
+            if header[-1]=="":
+                header.pop(-1)
+            header.append("wikidata")
+            header.append("wikipedia")
+            idcol=header.index("ID")
+            writer.writerow(header)
+            for row in reader:
+                if row[idcol] in wikidata:
+                    item,title=wikidata[row[idcol]]
+                    row.append(item)
+                    if title != "":
+                        title="en:{}".format(title)
+                    row.append(title)
+                else:
+                    row.append("")
+                    row.append("")
+                writer.writerow(row)
--- readme.md
+++ readme.md
@@ -1,7 +1,24 @@
 Project tracking adding data from http://gis-michigan.opendata.arcgis.com/datasets/minor-civil-divisions-cities-amp-townships-v17a
 to OpenStreetMap.
 
-After installing GDAL and retrieving ogr2osm, run this command to generate osm 
-data:
+Data prep requires GDAL and ogr2osm and curl.
 
-    ./ogr2osm/ogr2osm.py -t twptranslation.py  -o twp.osm /share/gis/micivil/
+Use mitwp.sparq to fetch wikidata ids:
+
+    curl -G https://query.wikidata.org/sparql? -o wikidata.csv -H "Accept: text/csv" --data-urlencode query="$(< mitwp.sparql)"
+
+Obtain township data from gnis and preprocess to format ogr understands.
+
+    python formatgnis.py mitownshipsgnis.csv mitownships.csv
+
+Use mitownships.vrt to do a spatial join adding the gnis id and name to 
+the boundaries.
+
+    ogr2ogr -sql "select t.*, g.ID, g.FeatureName, g.wikidata, g.wikipedia from civil t left join mitownships g on ST_CONTAINS(t.geometry, g.geometry)" -dialect SQLITE test.shp mitownships.vrt
+
+(this creates a bunch of warnings; the fields warned about aren't important)
+
+Generate osm data:
+
+    ./ogr2osm/ogr2osm.py -t twptranslation.py  -o twp.osm test.shp
+
