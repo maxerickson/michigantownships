db9e2cd34270a849793983f89cd196a4c3ca0cd6
Max Erickson <maxerickson@gmail.com>
2018-01-15 12:45:29 -0500

    Remove old county conflation code.

--- twptranslation.py
+++ twptranslation.py
@@ -5,33 +5,6 @@ import os
 
 __location__=os.path.dirname(os.path.realpath(__file__))
 
-
-# Special case townships that fail spacial join. The names of the missed
-# matchs are unique so no need for further special handling.
-townships={
-#~ "Caseville":"1626041",
-#~ "Eagle Harbor":"1626201",
-#~ "Fairhaven":"1626266",
-#~ "Glen Arbor":"1626359",
-#~ "Gore":"1626365",
-#~ "Houghton":"1626493",
-#~ "Lake":"1626573",
-#~ "Leelanau":"1626601",
-#~ "Leland":"1626603",
-#~ "Pointe Aux Barques":"1626921",
-#~ "Port Austin":"1626928",
-#~ "Rubicon":"1627014",
-#~ "Sims":"1627084",
-#~ "St James":"1627029",
-#~ "Suttons Bay":"1627143",
-}
-
-townshipmap=dict()
-with open(os.path.join(__location__,"mitownships.csv")) as data:
-    reader=csv.DictReader(data)
-    for row in reader:
-        townshipmap[row["ID"]]=row
-
 def filterTags(tags):
     if tags is None:
         return
@@ -47,21 +20,12 @@ def filterTags(tags):
             newtags["admin_level"]="7"
             newtags["boundary"]="administrative"
             # add gnis and wikidata tags.
-            gid=source=None
             if "ID" in tags and tags["ID"]!="":
-                gid=tags["ID"]
-                source=tags
-            elif tags["name"] in townships:
-                gid=townships[tags["name"]]
-                source=townshipmap[gid]
-                newtags["county"]=source["County"]
-            if gid is not None:
-                newtags["gnis:feature_id"]=gid
-            if source is not None:
-                if "wikidata" in source:
-                    newtags["wikidata"]=source["wikidata"]
-                if "wikipedia" in source:
-                    newtags["wikipedia"]=source["wikipedia"]
+                newtags["gnis:feature_id"]=tags["ID"]
+                if "wikidata" in tags:
+                    newtags["wikidata"]=tags["wikidata"]
+                if "wikipedia" in tags:
+                    newtags["wikipedia"]=tags["wikipedia"]
         if t=="City":
             newtags["admin_level"]="8"
             newtags["boundary"]="administrative"
